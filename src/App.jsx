1ea630fe59ca39bcc5706fcc7fb2cd9ec081573b
fady <fady44413@gmail.com>
2024-01-19 20:44:02 +0200

    changes has been made

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import UserInput from "./components/UserInput.jsx";
+import UserInput1 from "./components/UserInput.jsx";
+import { useState } from "react";
 import Header from "./components/Header.jsx";
+import Results from "./components/Results.jsx";
 function App() {
+  const [UserInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput1 UserInput={UserInput} onChange={handleChange} />
+      <Results input={UserInput} />
     </>
   );
 }
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,21 +1,6 @@
 import { useState } from "react";
 
-export default function UserInput() {
-  const [UserInput, setUserInput] = useState({
-    initialInvestment: 10000,
-    annualInvestment: 1200,
-    expectedReturn: 6,
-    duration: 10,
-  });
-  function handleChange(inputIdentifier, newValue) {
-    setUserInput((prevUserInput) => {
-      return {
-        ...prevUserInput,
-        [inputIdentifier]: newValue,
-      };
-    });
-  }
-
+export default function UserInput({ onChange, UserInput }) {
   return (
     <section id="user-input">
       <div className="input-group">
@@ -27,7 +12,7 @@ export default function UserInput() {
             required
             value={UserInput.initialInvestment}
             onChange={(event) =>
-              handleChange("initialInvestment", event.target.value)
+              onChange("initialInvestment", event.target.value)
             }
           />
         </p>
@@ -38,7 +23,7 @@ export default function UserInput() {
             required
             value={UserInput.annualInvestment}
             onChange={(event) =>
-              handleChange("annualInvestment", event.target.value)
+              onChange("annualInvestment", event.target.value)
             }
           />
         </p>
@@ -50,9 +35,7 @@ export default function UserInput() {
             type="number"
             required
             value={UserInput.expectedReturn}
-            onChange={(event) =>
-              handleChange("expectedReturn", event.target.value)
-            }
+            onChange={(event) => onChange("expectedReturn", event.target.value)}
           />
         </p>
 
@@ -62,7 +45,7 @@ export default function UserInput() {
             type="number"
             required
             value={UserInput.duration}
-            onChange={(event) => handleChange("duration", event.target.value)}
+            onChange={(event) => onChange("duration", event.target.value)}
           />
         </p>
       </div>
