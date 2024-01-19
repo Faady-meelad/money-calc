2bf5c59c3c36b1144b5319d1d5ec3c814a7c2b00
fady <fady44413@gmail.com>
2024-01-19 12:03:30 +0200

    the calc with react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import UserInput from "./components/UserInput.jsx";
+import Header from "./components/Header.jsx";
+function App() {
+  return (
+    <>
+      <Header />
+      <UserInput />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import logo from "../assets/investment-calculator-logo.png";
+
+export default function Header() {
+  return (
+    <header id="header">
+      <img src={logo} alt="money pic" />
+      <h1> money caalc</h1>
+    </header>
+  );
+}
